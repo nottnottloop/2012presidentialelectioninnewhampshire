d6174e9f396d7ec45bd41d39d14980e6c06334ca
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-02-18 10:37:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// window.addEventListener("DOMContentLoaded", event => {
+//   const audio = document.querySelector("audio");
+//   audio.volume = 0.2;
+//   audio.play();
+// });
+
+// var source = "./music.mp3"
+// var audio = document.createElement("audio");
+// //
+// audio.autoplay = true;
+// //
+// audio.load()
+// audio.addEventListener("load", function() { 
+// 		audio.play(); 
+// }, true);
+// audio.src = source;
+
+// var audioElement = document.createElement('audio');
+// audioElement.setAttribute('src', './music.mp3');
+// audioElement.load()
+// audioElement.addEventListener("load", function() { 
+//   audioElement.play(); 
+// }, true);
+
+// let html = 
+// `
+// <nav class="navbar navbar-expand px-4 navbarbg coolfont">
+// 	<span id="leftnav">
+// 	<a id="logo" href="./index.html"><img src="icon.png" width="50px"></a>
+// 	<a style="color:red;" href="./index.html">Basics</a>
+// 	<a style="color:blue;" href="./funfacts.html">Fun Facts</a>
+// 	<a style="color:red;" href="./endorsements.html">Endorsements</a>
+// 	<a style="color:blue;" href="./end_result.html">Results</a>
+// 	</span>
+// 	<span id="rightnav">
+// 	<p style="display: inline;" id="thetitle">The 2012 Presidential Election in New Hampshire</p>
+// 	<img style="display:inline;" src="newhampshire.png" height="50px">
+// 	</span>
+// </nav>
+// `;
+
+let html = 
+`
+<nav class="navbar navbar-expand px-4 navbarbg coolfont">
+	<span id="leftnav">
+	<a id="logo" href="./index.html"><img src="icon.png" width="50px"></a>
+	<a style="color:red;" href="./index.html">Basics</a>
+	<a style="color:blue;" href="./funfacts.html">Fun Facts</a>
+	<a style="color:red;" href="./endorsements.html">Endorsements</a>
+	<a style="color:blue;" href="./end_result.html">Results</a>
+	</span>
+	<span id="rightnav">
+	<p style="display: inline;" class="wackytext">The 2012 Presidential Election in New Hampshire</p>
+	<img style="display:inline;" src="newhampshire.png" height="50px">
+	</span>
+</nav>
+`;
+
+let audio = 
+`
+<iframe src="./music.mp3" type="audio/mp3" allow="autoplay" id="audio" style="display:inline;"></iframe>
+<audio autoplay>
+`
+// let footer = 
+// `
+// <h5>Website sponsored by Pancakeland County Council</h5>
+// `;
+
+document.getElementById('nav').innerHTML = html;
+document.getElementById('americandad').innerHTML = audio;
+var aud = document.getElementById("americandad");
+aud.volume = 0.01;
+
